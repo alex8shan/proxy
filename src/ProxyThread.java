f314b5700ea6be10832d290897a527ac2d4665fa
Minghao Shan <minghao8shan@gmail.com>
2018-04-25 01:35:49 -0500

    Finished commenting

--- src/ProxyThread.java
+++ src/ProxyThread.java
@@ -6,78 +6,114 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ProxyThread extends Thread{
-	private final Socket clientSocket;
-	private final ConcurrentHashMap<String, String> cache;
-	
-	public ProxyThread(Socket i, ConcurrentHashMap<String, String> cache) {
-		clientSocket = i;
-		this.cache = cache;
-	}
-	
-	public void run(){
-        String req = "", url="";
-		try {
-			InputStreamReader isr =  new InputStreamReader(clientSocket.getInputStream());
-	        BufferedReader reader = new BufferedReader(isr);
-	        String line = reader.readLine();        
-	        String firstLine = line;
-	        while (!line.isEmpty()) {
-	            req += line;
-	            line = reader.readLine();
-	        }
-	        String method = firstLine.substring(0, firstLine.indexOf(' '));
-	        	url = firstLine.substring(firstLine.indexOf(' ') + 1, firstLine.lastIndexOf(' '));
-	        	
-	        	if(!method.toUpperCase().equals("GET")) {
-	        		throw new Exception(method + " method is not yet implemented!");
-	        	}
-	        	
-	        	if(!url.startsWith("http")) {
-	        		throw new Exception("Invalid HTTP request format");
-	        	}
-	        	String result = "";
-	        	if (checkCache(url)) {
-	        		System.out.println("Sending from cache");
-	        		result = fetchFromCache(url);
-	        	}
-	        	else {
-	        		result = getWebpage(url);
-	        	}
-	        	addToCache(url, result);
-	        	System.out.println("Sending " + clientSocket.getInetAddress() + "\n" + result);
-	        	clientSocket.getOutputStream().write(result.getBytes());
-	        	
-		} catch(Exception e) {
-			System.out.println(req);
-			System.out.println(url);
-			e.printStackTrace();
-		}
-	}
-	
-	private boolean checkCache(String url) {
-		return cache.containsKey(url);
-	}
-	
-	private void addToCache(String url, String response) {
-		cache.put(url, response);
-	}
-	
-	private String fetchFromCache(String url) {
-		return cache.get(url);
-	}
-	
-	private String getWebpage(String url) throws IOException{
-		URL result = new URL(url);
+/**
+ * This is the multi-threading part
+ * @author  Minghao Shan, Anh Nguyen, Andrew Rodeghero, Harshavardhan Reddy Madduri
+ * @version 04/22/2018
+ */
+public class ProxyThread extends Thread {
+    private final Socket clientSocket;
+    private final ConcurrentHashMap<String, String> cache;
+
+    /**
+     * Constructor
+     * @param s     Socket
+     * @param cache Cache
+     */
+    public ProxyThread(Socket s, ConcurrentHashMap<String, String> cache) {
+        clientSocket = s;
+        this.cache = cache;
+    }
+
+    /**
+     * Runs when a new thread starts
+     */
+    public void run() {
+        String req = "", url = "";
+        try {
+            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            String firstLine = line;
+            while (!line.isEmpty()) {
+                req += line;
+                line = reader.readLine();
+            }
+            //get the header of request
+            String header = firstLine.substring(0, firstLine.indexOf(' '));
+
+            url = firstLine.substring(firstLine.indexOf(' ') + 1, firstLine.lastIndexOf(' '));
+            //return not implemented error if header is not get
+            if (!header.toUpperCase().equals("GET")) {
+                throw new Exception("501 Not Implemented");
+            }
+            //
+            if (!url.startsWith("http")) {
+                throw new Exception("400 Bad Request");
+            }
+
+            String result = "";
+            if (checkCache(url)) {
+                System.out.println("Sending from cache");
+                result = fetchFromCache(url);
+            } else {
+                result = getWebpage(url);
+            }
+            addToCache(url, result);
+            System.out.println("Sending " + clientSocket.getInetAddress() + "\n\nResponse:\n" + result);
+            clientSocket.getOutputStream().write(result.getBytes());
+
+        } catch (Exception e) {
+            System.out.println(req);
+            System.out.println(url);
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Checks if request was already stored in cache
+     * @param url   The key of cache; the request from client
+     * @return      True if the request exist and vice versa
+     */
+    private boolean checkCache(String url) {
+        return cache.containsKey(url);
+    }
+
+    /**
+     * Add the key value pair to cache
+     * @param url       The key of cache; the request from client
+     * @param response  The value of cache; the response from host server
+     */
+    private void addToCache(String url, String response) {
+        cache.put(url, response);
+    }
+
+    /**
+     * Returns the respond corresponding to the url
+     * @param url   The request from client
+     * @return      The response from host server
+     */
+    private String fetchFromCache(String url) {
+        return cache.get(url);
+    }
+
+    /**
+     * returns the response from host server
+     * @param url   The request from client
+     * @return      The response from host server
+     */
+    private String getWebpage(String url) throws IOException {
+        URL result = new URL(url);
         URLConnection yc = result.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(
-                                    yc.getInputStream()));
+                yc.getInputStream()));
         String response = "";
         String inputLine;
-        while ((inputLine = in.readLine()) != null) 
+        while ((inputLine = in.readLine()) != null){
+            inputLine += "\n";
             response += inputLine;
-        
+        }
         return response;
-	}
+    }
 
 }
